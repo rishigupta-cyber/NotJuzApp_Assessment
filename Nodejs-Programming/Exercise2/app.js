67622147a1a5d6e77e37a3978635dafeee4157ca
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-06-04 23:37:32 +0530

    Delete Nodejs-Programming directory

--- Nodejs-Programming/Exercise2/app.js
+++
@@ -1,5 +0,0 @@
-const utils = require('./stringUtils');
-
-console.log("Capitalized:", utils.capitalize("nodejs"));
-console.log("Reversed:", utils.reverse("backend"));
-console.log("Vowel Count:", utils.countVowels("engineering"));
