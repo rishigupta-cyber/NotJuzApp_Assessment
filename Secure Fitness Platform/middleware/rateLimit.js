f23f19ec1c7f5213cac8951c83ac516809f8c791
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-06-04 23:37:55 +0530

    Delete Secure Fitness Platform directory

--- Secure Fitness Platform/middleware/rateLimit.js
+++
@@ -1,8 +0,0 @@
-const rateLimit=require("express-rate-limit")
-
-const limiter=rateLimit({
-windowMs:60*1000,
-max:20
-})
-
-module.exports=limiter
--- Secure Fitness Platform/utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt=require("jsonwebtoken")
-
-exports.generateAccessToken=(user)=>{
-return jwt.sign(user,process.env.JWT_SECRET,{expiresIn:"15m"})
-}
-
-exports.generateRefreshToken=(user)=>{
-return jwt.sign(user,process.env.REFRESH_SECRET,{expiresIn:"7d"})
-}
